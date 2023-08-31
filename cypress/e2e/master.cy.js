840ae5278b935dc51ea326f77d60d73ddcaf6fb9
JRafaCand <r.candilejo@gmail.com>
2023-08-31 12:45:28 -0500

    Add selector section

--- cypress/e2e/master.cy.js
+++ cypress/e2e/master.cy.js
@@ -1,5 +1,5 @@
 import 'cypress-plugin-tab'; 
-     
+require('xpath_cypress');     
 
   describe('template spec', () => {
   it('passes', () => {
@@ -36,5 +36,26 @@ import 'cypress-plugin-tab';
     cy.get('#searchBox').should('be.visible').type('Gonza Prueba').type('{enter}')
     cy.wait(500)
     cy.get('#delete-record-4').should('be.visible').click({force: true})  
+  })
+  it.only('Id for Selector', () => {//only, for this option run only this test
+    cy.visit('https://demoqa.com/automation-practice-form')
+    cy.wait(500)
+    cy.get ("[placeholder='First Name']").should('be.visible').type('Rafa Prueba')//selector by placeholder
+    cy.wait(500)
+    cy.get (".custom-control-label").should("be.visible").contains("Female").click()//selector by class and contains
+    cy.wait(500)
+    cy.get('label.custom-control-label').should('be.visible').contains('Other').click({force: true})//selector by label and contains
+    cy.wait(500)
+    cy.get('input#userNumber').should('be.visible').type('1234567890')//selector by id (click left button in the element and select copy selector)
+    cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').should('be.visible').click()//selector by id and nth-child
+    cy.get('label.custom-control-label').should('be.visible').contains('Sports').click({force: true})//selector by label and contains
+    cy.wait(500)
+    cy.get('label.custom-control-label').should('be.visible').contains('Reading').click({force: true})//selector by label and contains
+    cy.get('input#lastName[type="text"]').should('be.visible').type('Rafa Prueba')//selector by id and type and attribute  
+    cy.xpath('//*[@id="userEmail"]').should('be.visible').type('rafa@prueba.com')
+    cy.wait(500)
+    cy.xpath('//div[text()="Book Store Application"]').should('be.visible').click({force: true})//selector by text and xpath
+    cy.wait(500)
+    cy.xpath('//span[contains(text(),"Lo")]').should('be.visible').click({force: true})//selector by contains and text and xpath  
   })
 })
