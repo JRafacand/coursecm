05c5a453dd977db0ab964f64bbbf518f59ce60bd
JRafaCand <r.candilejo@gmail.com>
2023-09-14 13:31:44 -0500

    Alert exercises

--- cypress/e2e/alert.cy.js
+++ cypress/e2e/alert.cy.js
@@ -3,6 +3,18 @@ require('@4tw/cypress-drag-drop');
 require('xpath_cypress');
 
 describe('cypress course', () => {
-
-
+    it('alert', () => {
+    cy.visit('https://demoqa.com/alerts')
+    cy.title().should('eq', 'DEMOQA')
+    /* cy.get('#alertButton').click({force: true})
+    cy.wait(500)
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal('You clicked a button')
+    }) */
+    cy.get('#timerAlertButton').click({force: true})
+    cy.wait(5000)
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal('This alert appeared after 5 seconds')
+    })    
+})
 })
